a8d19df0c9dcfc2cf08b2ac278412bedc0dcb168
Kohit <kohitprajapat@gmail.com>
2025-01-28 22:18:50 +0530

    node js

---
+++ js/index.js
@@ -0,0 +1,72 @@
+
+let curr = new Date();
+console.log(curr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Humna{
+//     age = 18;
+//     wt = 68;
+//     ht = 167;
+
+//     walking(){ 
+//         console.log("Walking...");
+//     }
+
+//     running(){
+//         console.log("Running...");
+//     }
+
+//     eating() {
+//         console.log("Eating...");
+//     }
+
+//     killing = () =>{
+//         console.log("Killing...");
+//     }
+// }
+
+// let obj = new Humna();
+// obj.eating();
